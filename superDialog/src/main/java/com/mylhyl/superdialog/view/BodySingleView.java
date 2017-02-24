bcfb65f1f77336f42a175ad57bae97aa9075c963
hupei <hupei132@qq.com>
2017-02-24 11:49:32 +0800

    修改没有设置title时无圆角边框

--- superDialog/src/main/java/com/mylhyl/superdialog/view/BodySingleView.java
+++ superDialog/src/main/java/com/mylhyl/superdialog/view/BodySingleView.java
@@ -9,25 +9,43 @@ import com.mylhyl.superdialog.res.drawable.BgBtn;
  * Created by hupei on 2016/3/8 19:30.
  */
 class BodySingleView extends SuperTextView {
+    private Controller.Params mParams;
 
     public BodySingleView(Context context, Controller.Params params) {
         super(context);
-        initData(params);
+        mParams = params;
+        initData();
     }
 
-    private void initData(Controller.Params params) {
-        if (params.mFooterNegative == null && params.mFooterPositive == null) {
-            int radius = params.mRadius;
-            setBackgroundDrawable(new BgBtn(0, 0, radius, radius, params.mBackgroundColor));
+    private void initData() {
+        if (mParams.mProviderHeader != null && mParams.mFooterNegative == null
+                && mParams.mFooterPositive == null) {
+            int radius = mParams.mRadius;
+            setBackgroundDrawable(new BgBtn(0, 0, radius, radius, mParams.mBackgroundColor));
+        } else if (mParams.mProviderHeader == null && (mParams.mFooterNegative != null
+                || mParams.mFooterPositive != null)) {
+            int radius = mParams.mRadius;
+            setBackgroundDrawable(new BgBtn(radius, radius, 0, 0, mParams.mBackgroundColor));
+        } else if (mParams.mFooterNegative == null && mParams.mFooterPositive == null
+                && mParams.mProviderHeader == null) {
+            int radius = mParams.mRadius;
+            setBackgroundDrawable(new BgBtn(radius, radius, radius, radius, mParams
+                    .mBackgroundColor));
         } else {
-            setBackgroundColor(params.mBackgroundColor);
+            setBackgroundColor(mParams.mBackgroundColor);
         }
-        ProviderContent providerContent = params.mProviderContent;
+        ProviderContent providerContent = mParams.mProviderContent;
         if (providerContent == null) return;
         setText((String) providerContent.getItems());
         setTextSize(providerContent.getTextSize());
         setTextColor(providerContent.getTextColor());
         int[] padding = providerContent.getPadding();
         setAutoPadding(padding[0], padding[1], padding[2], padding[3]);
+    }
+
+    public void refreshText() {
+        ProviderContent providerContent = mParams.mProviderContent;
+        if (providerContent == null) return;
+        setText((String) providerContent.getItems());
     }
 }
